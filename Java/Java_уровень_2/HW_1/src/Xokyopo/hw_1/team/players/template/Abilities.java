d3757c1a90c3dea97f6ac58a0383628a67f85fb2
Alexey <enslaver@list.ru>
2019-11-19 00:01:58 +0300

    Домашнее задание №2

--- Java/Java_уровень_2/HW_1/src/Xokyopo/hw_1/team/players/template/Abilities.java
+++
@@ -1,7 +0,0 @@
-package Xokyopo.hw_1.team.players.template;
-
-public interface Abilities {
-    boolean isCanRun(double _runDistance);
-    boolean isCanJump(double _jumpHeight);
-    boolean isCanSwim(double _swimDistance);
-}
---
+++ Java/Java_уровень_2/HW_2/src/Xokyopo/hw_2/Main.java
@@ -0,0 +1,8 @@
+package Xokyopo.hw_2;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}

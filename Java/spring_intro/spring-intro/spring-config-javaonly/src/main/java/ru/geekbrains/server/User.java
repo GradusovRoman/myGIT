326cf4b20e9735c9357df9a11e0e4561428bae41
Alexey <enslaver@list.ru>
2020-08-25 12:26:09 +0300

    created 3 similar module for practice with spring configuration (#73)
    Lesson 1. Introduction to Spring

---
+++ Java/spring_intro/spring-intro/spring-config-javaonly/src/main/java/ru/geekbrains/server/User.java
@@ -0,0 +1,26 @@
+package ru.geekbrains.server;
+
+public class User {
+
+    private final int id;
+    private final String login;
+    private final String password;
+
+    public User(int id, String login, String password) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
---
+++ Java/spring_intro/spring-intro/spring-config-xmlonly/src/main/java/ru/geekbrains/server/Main.java
@@ -0,0 +1,26 @@
+package ru.geekbrains.server;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.geekbrains.server.persistance.UserRepository;
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
+
+        try {
+            UserRepository userRepository = context.getBean(UserRepository.class);
+            if (userRepository.getAllUsers().size() == 0) {
+                userRepository.insert(new User(-1, "ivan", "123"));
+                userRepository.insert(new User(-1, "petr", "345"));
+                userRepository.insert(new User(-1, "julia", "789"));
+            }
+        } catch (SQLException e) {
+//            ignore
+        }
+
+        context.getBean(ChatServer.class).start(7777);
+    }
+}

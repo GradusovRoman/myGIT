3bae00f86c76799f5fd192406f357c61435f037c
Alexey <enslaver@list.ru>
2020-09-21 15:52:59 +0300

    выполнил форматирование добавил сортировку по столбцам добавил разбиение на страницы

--- Java/spring_intro/spring-intro/spring-mvc/src/main/java/ru/geekbrains/thirdquarter/springintro/mvc/app/ui/mvc/spring/controllers/ProductController.java
+++ Java/spring_intro/spring-intro/spring-mvc/src/main/java/ru/geekbrains/thirdquarter/springintro/mvc/app/ui/mvc/spring/controllers/ProductController.java
@@ -1,6 +1,9 @@
 package ru.geekbrains.thirdquarter.springintro.mvc.app.ui.mvc.spring.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +13,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import ru.geekbrains.thirdquarter.springintro.mvc.app.domain.ProductService;
 import ru.geekbrains.thirdquarter.springintro.mvc.app.domain.entities.Product;
 
+import java.util.Optional;
+
 @Controller
 public class ProductController {
-    private ProductService service;
+    private static final int PAGE_PRODUCT_LIMIT = 5;
+    private final ProductService service;
 
     @Autowired
     public ProductController(ProductService service) {
@@ -23,13 +29,23 @@ public class ProductController {
     public String showAllProducts(
             Model model,
             @RequestParam(value = "min", required = false) Integer min,
-            @RequestParam(value = "max", required = false) Integer max) {
-        model.addAttribute("products", this.service.getAll());
+            @RequestParam(value = "max", required = false) Integer max,
+            @RequestParam(value = "page") Optional<Integer> page,
+            @RequestParam(value = "sortBy", required = false) String sortBY,
+            @RequestParam(value = "sort", required = false) String sort
+    ) {
+
+        Pageable pageable = (sortBY == null || sortBY.isEmpty()) ?
+                PageRequest.of(page.orElse(1) - 1, PAGE_PRODUCT_LIMIT) :
+                PageRequest.of(page.orElse(1) - 1, PAGE_PRODUCT_LIMIT,
+                        (sort.equals("up") ? Sort.by(sortBY).ascending() : Sort.by(sortBY).descending()));
+
         if (min == null && max == null) {
-            model.addAttribute("products", this.service.getAll());
+            model.addAttribute("page", this.service.getAll(pageable));
         } else {
-            model.addAttribute("products", this.service.getAllByPriceFilter(min, max));
+            model.addAttribute("page", this.service.getAllByPriceFilter(min, max, pageable));
         }
+
         return "products";
     }
 
@@ -37,13 +53,12 @@ public class ProductController {
     @GetMapping("/del/{id}")
     public String deleteProduct(@PathVariable long id, Model model) {
         this.service.delById(id);
-        model.addAttribute("products", this.service.getAll());
         return "redirect:/";
     }
 
     @GetMapping("/show/{id}")
     public String showProduct(@PathVariable long id, Model model) {
-        model.addAttribute("product", (id >0 ) ? this.service.getById(id) : new Product());
+        model.addAttribute("product", (id > 0) ? this.service.getById(id) : new Product());
         return "product";
     }
 

147c3657f4428ba322823876fc9b9b62a796c040
Alexey <enslaver@list.ru>
2020-10-18 23:56:05 +0300

    added Been for passwordEncoded

--- Java/spring_intro/spring-intro/spring-boot/src/main/java/ru/geekbrains/thirdquarter/springintro/springboot/Application.java
+++ Java/spring_intro/spring-intro/spring-boot/src/main/java/ru/geekbrains/thirdquarter/springintro/springboot/Application.java
@@ -2,9 +2,16 @@ package ru.geekbrains.thirdquarter.springintro.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import ru.geekbrains.thirdquarter.springintro.springboot.app.ExtendedBCryptPasswordEncoder;
 
 @SpringBootApplication
 public class Application {
+
+	@Bean
+	public ExtendedBCryptPasswordEncoder getPasswordEncoder() {
+		return new ExtendedBCryptPasswordEncoder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
